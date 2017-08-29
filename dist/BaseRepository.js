8f6eed3d0850e9ade94e24f5fb0956a490f8294e
Alan Marcell <alan.marcell@accenture.com>
2017-08-28 23:50:00 -0300

    Compile target  node 6

--- dist/BaseRepository.js
+++ dist/BaseRepository.js
@@ -13,25 +13,47 @@ var _ramda2 = _interopRequireDefault(_ramda);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-const getDb = async url => await _mongodb.MongoClient.connect(url);
-const getDbCollection = _ramda2.default.curry((db, collectionName) => db.collection(collectionName));
-const createRepository = exports.createRepository = _ramda2.default.curry(async (url, collectionName) => {
-    const db = await getDb(url);
-    const collection = getDbCollection(db, collectionName);
-    return {
-        db,
-        collectionName,
-        getDbCollection,
-        save: save(collection),
-        getById: getById(collection),
-        getByIds: getByIds(collection),
-        find: find(collection)
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+const getDb = (() => {
+    var _ref = _asyncToGenerator(function* (url) {
+        return yield _mongodb.MongoClient.connect(url);
+    });
+
+    return function getDb(_x) {
+        return _ref.apply(this, arguments);
     };
-});
-const save = _ramda2.default.curry(async (collection, entity) => {
-    const result = await collection.replaceOne({ _id: entity.id }, entity, { upsert: true });
-    return result.ops[0];
-});
+})();
+const getDbCollection = _ramda2.default.curry((db, collectionName) => db.collection(collectionName));
+const createRepository = exports.createRepository = _ramda2.default.curry((() => {
+    var _ref2 = _asyncToGenerator(function* (url, collectionName) {
+        const db = yield getDb(url);
+        const collection = getDbCollection(db, collectionName);
+        return {
+            db,
+            collectionName,
+            getDbCollection,
+            save: save(collection),
+            getById: getById(collection),
+            getByIds: getByIds(collection),
+            find: find(collection)
+        };
+    });
+
+    return function (_x2, _x3) {
+        return _ref2.apply(this, arguments);
+    };
+})());
+const save = _ramda2.default.curry((() => {
+    var _ref3 = _asyncToGenerator(function* (collection, entity) {
+        const result = yield collection.replaceOne({ _id: entity.id }, entity, { upsert: true });
+        return result.ops[0];
+    });
+
+    return function (_x4, _x5) {
+        return _ref3.apply(this, arguments);
+    };
+})());
 const getById = _ramda2.default.curry((collection, id) => collection.findOne({ _id: id }));
 const getByIds = _ramda2.default.curry((collection, ids) => collection.find({ _id: { $in: ids } }).toArray());
 // TODO add FindOptions type to options and remove any when DefinitelyTypes
--- dist/BaseRepository.test.js
+++ dist/BaseRepository.test.js
@@ -4,56 +4,58 @@ var _ptzAssert = require('ptz-assert');
 
 var _index = require('./index');
 
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
 const MONGO_URL = 'mongodb://localhost:27017/ptz-core-repo';
 var baseRepository;
 /* tslint:disable:no-string-literal */
 describe('BaseRepository', () => {
-    beforeEach(async () => {
-        baseRepository = await (0, _index.createRepository)(MONGO_URL, 'test-collection');
-    });
+    beforeEach(_asyncToGenerator(function* () {
+        baseRepository = yield (0, _index.createRepository)(MONGO_URL, 'test-collection');
+    }));
     describe('save', () => {
-        it('insert', async () => {
+        it('insert', _asyncToGenerator(function* () {
             const entity = {
                 id: 'testid',
                 name: 'testName'
             };
-            await baseRepository.save(entity);
-            const entityDb = await baseRepository.getById(entity.id);
+            yield baseRepository.save(entity);
+            const entityDb = yield baseRepository.getById(entity.id);
             (0, _ptzAssert.ok)(entityDb);
             (0, _ptzAssert.equal)(entityDb.id, entity.id);
-        });
-        it('update', async () => {
+        }));
+        it('update', _asyncToGenerator(function* () {
             const entity = {
                 id: 'testid',
                 name: 'testName'
             };
             entity['name'] = 'teste';
-            await baseRepository.save(entity);
+            yield baseRepository.save(entity);
             const newName = 'teste2';
             entity['name'] = newName;
-            await baseRepository.save(entity);
-            const entityDb = await baseRepository.getById(entity.id);
+            yield baseRepository.save(entity);
+            const entityDb = yield baseRepository.getById(entity.id);
             (0, _ptzAssert.ok)(entityDb);
             (0, _ptzAssert.equal)(entityDb.id, entity.id);
             (0, _ptzAssert.equal)(entityDb['name'], newName);
-        });
+        }));
     });
     describe('find', () => {
-        it('by Email', async () => {
+        it('by Email', _asyncToGenerator(function* () {
             const entity = {
                 id: 'testid',
                 name: 'testName'
             };
             entity['email'] = 'angeloocana@gmail.com';
-            await baseRepository.save(entity);
+            yield baseRepository.save(entity);
             const query = {
                 email: entity['email']
             };
-            const entityDb = await baseRepository.find(query, { limit: 1 });
+            const entityDb = yield baseRepository.find(query, { limit: 1 });
             (0, _ptzAssert.ok)(entityDb[0]);
             (0, _ptzAssert.equal)(entityDb[0]['email'], entity['email']);
-        });
-        it('limit by 3', async () => {
+        }));
+        it('limit by 3', _asyncToGenerator(function* () {
             for (let i = 0; i <= 6; i++) {
                 const entity = {
                     name: 'testName',
@@ -61,17 +63,17 @@ describe('BaseRepository', () => {
                 };
                 entity['testLimit'] = true;
                 entity['i'] = i;
-                await baseRepository.save(entity);
+                yield baseRepository.save(entity);
             }
             const query = {
                 testLimit: true
             };
-            const entitiesDb = await baseRepository.find(query, { limit: 3 });
+            const entitiesDb = yield baseRepository.find(query, { limit: 3 });
             (0, _ptzAssert.equal)(entitiesDb.length, 3);
-        });
+        }));
     });
     describe('getByIds', () => {
-        it('get 3 entities by ids', async () => {
+        it('get 3 entities by ids', _asyncToGenerator(function* () {
             const entities = [];
             for (let i = 0; i <= 6; i++) {
                 const entity = {
@@ -80,11 +82,11 @@ describe('BaseRepository', () => {
                 };
                 entity['i'] = i;
                 entities.push(entity);
-                await baseRepository.save(entity);
+                yield baseRepository.save(entity);
             }
-            const entitiesDb = await baseRepository.getByIds([entities[0].id, entities[1].id, entities[2].id]);
+            const entitiesDb = yield baseRepository.getByIds([entities[0].id, entities[1].id, entities[2].id]);
             (0, _ptzAssert.equal)(entitiesDb.length, 3);
-        });
+        }));
     });
 });
 /* tslint:enable:no-string-literal */
